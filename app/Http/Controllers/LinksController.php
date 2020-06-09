6cac4a92c7e157a958b0b10d4de951b7bae4f05b
Ferdroid8080 <andresf@ferudinato.com>
2020-06-08 20:32:42 -0500

    added agent library and request to api for getting info of the visitors

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Links;
+use App\LinksTracker;
 use Illuminate\Http\Request;
+use Jenssegers\Agent\Agent;
+use Log;
 
 class LinksController extends Controller
 {
@@ -13,9 +16,43 @@ class LinksController extends Controller
     {
         $link = Links::where('shortened_url', $query)->first();
 
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $ipInfo = file_get_contents("http://ip-api.com/json/" . $ip);
+        $ipInfo = json_decode($ipInfo);
+
+        $agent = new Agent();
+        $tracker = [
+            'ip' => $ip,
+            'browser' => $agent->browser(),
+            'os' => $agent->platform(),
+            'device' => $agent->device(),
+            'robot' => $agent->robot(),
+            'referer' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '',
+            'user_agent' => $_SERVER['HTTP_USER_AGENT']
+        ];
+
+        if ($ipInfo->status == 'success') {
+            $tracker['country'] = $ipInfo->country;
+            $tracker['city']  = $ipInfo->city;
+            $tracker['region'] = $ipInfo->region;
+            $tracker['region_code'] = $ipInfo->regionName;
+            $tracker['isp'] = $ipInfo->isp;
+            $tracker['country_code'] = $ipInfo->countryCode;
+            $tracker['timezone'] = $ipInfo->timezone;
+        }
+
         if ($link) {
             $link->clicks = $link->clicks + 1;
             $link->save();
+
+            foreach($tracker as $i => $v) {
+                $linkTracker = new LinksTracker();
+                $linkTracker->dyn_field = $i;
+                $linkTracker->dyn_value_field = $v;
+                $linkTracker->links_id = $link->id;
+                $linkTracker->save();
+            }
+
             return redirect($link->orig_url);
         } else {
             abort(404, 'Not found');
