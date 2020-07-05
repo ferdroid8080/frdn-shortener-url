b8b5f9fc2bba89f919fce0e9299c4fcf690a75b6
Ferdroid8080 <andresf@ferudinato.com>
2020-07-05 17:15:48 -0500

    Button component now receives a onclick event and a disabled prop

--- resources/js/components/UI/Button.js
+++ resources/js/components/UI/Button.js
@@ -4,7 +4,11 @@ import React from 'react'
 const button = props => {
 
     return (
-        <button className={props.classes.join(' ')}>{props.children}</button>
+        <button
+            className={props.classes.join(' ')}
+            onClick={props.clicked} disabled={props.disabled}>
+                {props.children}
+        </button>
     )
 }
 
